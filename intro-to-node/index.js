f65d034b7d4225129654c2970f49d05af4b6f095
SmritiSharma1209 <79032799+SmritiSharma1209@users.noreply.github.com>
2021-06-17 09:08:31 +0530

    adding initial files

---
+++ intro-to-node/index.js
@@ -0,0 +1,10 @@
+
+ var superheroes= require("superheroes");
+ var supervillains=require("supervillains");
+
+ var mySuperHero=superheroes.random();
+ var myVillain= supervillains.random();
+
+
+console.log(mySuperHero);
+console.log(myVillain);
